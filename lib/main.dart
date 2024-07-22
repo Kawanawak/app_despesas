ede6c0e38c33aeb5089b38ed06a86779a9814859
Kawanawak <kawan929305@gmail.com>
2024-07-22 19:59:59 -0300

    3 commit

--- lib/main.dart
+++ lib/main.dart
@@ -35,21 +35,24 @@ class MyHomePage extends StatelessWidget {
           title: Text('Despesas Pessoais'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('Gráfico'),
-                elevation: 5,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('Gráfico'),
+                  elevation: 5,
+                ),
               ),
-            ),
-            Card(
-              child: Text('Lista de Transaçôes'),
-            )
-          ],
-        ));
+              Column(
+                children: _transections.map((tr) {
+                  return Card(
+                    child: Text(tr.title),
+                  );
+                }).toList(),
+              )
+            ]));
   }
 }
