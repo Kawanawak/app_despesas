6a99bd108ddcd1b143490188381fabfe88861295
Kawanawak <kawan929305@gmail.com>
2024-07-22 19:04:54 -0300

    2 commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:app_despesas/models/transection.dart';
+import 'package:flutter/material.dart';
+
+main() => runApp(DespesasApp());
+
+class DespesasApp extends StatelessWidget {
+  const DespesasApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transections = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Ténis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 210.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('Lista de Transaçôes'),
+            )
+          ],
+        ));
+  }
+}
